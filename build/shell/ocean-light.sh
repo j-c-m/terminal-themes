8175c687a249828c651fec25aba141c7a025a94f
Jesse Miller <jmiller@jmiller.com>
2026-08-16 12:34:01 -0600

    Add Kempson Ocean and Spacegray Ocean Light; make Spacegray a combined theme.

--- build/shell/ocean-light.sh
+++ build/shell/ocean-light.sh
@@ -21,7 +21,7 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # SPDX-License-Identifier: MIT
-# Theme:    Spacegray Light
+# Theme:    Ocean Light
 # Mode:     light
 # Source:   Spacegray (https://github.com/SublimeText/Spacegray/)
 
@@ -53,47 +53,47 @@ print_linux() {
 }
 
 do_osc() {
-    print_osc4 0 "34/3d/46"
+    print_osc4 0 "df/e1/e8"
     print_osc4 1 "c8/58/63"
     print_osc4 2 "6f/98/4c"
     print_osc4 3 "90/60/4f"
     print_osc4 4 "59/86/b6"
     print_osc4 5 "8b/5b/82"
     print_osc4 6 "5c/a8/a5"
-    print_osc4 7 "c0/c5/ce"
-    print_osc4 8 "65/73/7e"
+    print_osc4 7 "4f/5b/66"
+    print_osc4 8 "a7/ad/ba"
     print_osc4 9 "cf/6e/77"
     print_osc4 10 "7a/a7/54"
     print_osc4 11 "eb/cb/8b"
     print_osc4 12 "6c/94/be"
     print_osc4 13 "99/64/8f"
     print_osc4 14 "6d/b1/ae"
-    print_osc4 15 "ef/f1/f5"
+    print_osc4 15 "2b/30/3b"
 
-    print_osc_rgb 10 "65/73/7e"
-    print_osc_rgb 11 "df/e1/e8"
-    print_osc_rgb 12 "65/73/7e"
-    print_osc_rgb 17 "65/73/7e"
-    print_osc_rgb 19 "df/e1/e8"
+    print_osc_rgb 10 "4f/5b/66"
+    print_osc_rgb 11 "ef/f1/f5"
+    print_osc_rgb 12 "4f/5b/66"
+    print_osc_rgb 17 "4f/5b/66"
+    print_osc_rgb 19 "ef/f1/f5"
 }
 
 do_linux() {
-    print_linux 0 "#343d46"
+    print_linux 0 "#dfe1e8"
     print_linux 1 "#c85863"
     print_linux 2 "#6f984c"
     print_linux 3 "#90604f"
     print_linux 4 "#5986b6"
     print_linux 5 "#8b5b82"
     print_linux 6 "#5ca8a5"
-    print_linux 7 "#c0c5ce"
-    print_linux 8 "#65737e"
+    print_linux 7 "#4f5b66"
+    print_linux 8 "#a7adba"
     print_linux 9 "#cf6e77"
     print_linux 10 "#7aa754"
     print_linux 11 "#ebcb8b"
     print_linux 12 "#6c94be"
     print_linux 13 "#99648f"
     print_linux 14 "#6db1ae"
-    print_linux 15 "#eff1f5"
+    print_linux 15 "#2b303b"
 }
 
 case "$TERM" in
